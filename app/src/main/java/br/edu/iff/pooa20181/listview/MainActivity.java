f1e65323cc58ebbb65288ed45f60a66dc1669755
Lucas <lucasmisidoro@gmail.com>
2018-06-04 21:13:15 -0300

    Criacao da Lista de Equipes

--- app/src/main/java/br/edu/iff/pooa20181/listview/MainActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/listview/MainActivity.java
@@ -2,6 +2,8 @@ package br.edu.iff.pooa20181.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lista = (ListView) findViewById(R.id.LvEquipes);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.equipes, android.R.layout.simple_list_item_1);
+        lista.setAdapter(adapter);
     }
 }
